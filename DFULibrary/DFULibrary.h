88d06dbed822573ab9cfc90a49bb3495a7d1225f
Nate Stedman <natesm@gmail.com>
2016-12-21 10:29:26 -0500

    Cleanup for Carthage usage.

---
+++ DFULibrary/DFULibrary.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for DFULibrary.
+FOUNDATION_EXPORT double DFULibraryVersionNumber;
+
+//! Project version string for DFULibrary.
+FOUNDATION_EXPORT const unsigned char DFULibraryVersionString[];
+
+#import <DFULibrary/IntelHex2BinConverter.h>
